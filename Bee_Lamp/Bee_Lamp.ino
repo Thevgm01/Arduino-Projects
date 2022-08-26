f74d07dc2e34f31b5442446e66a7106be06e5d3b
Steven Dunn <thevgm01@gmail.com>
2022-08-26 15:06:15 -0500

    Added sound input, more frequent scanning

--- Bee_Lamp/Bee_Lamp.ino
+++ Bee_Lamp/Bee_Lamp.ino
@@ -14,11 +14,11 @@
 // 2:3
 // 2*2+3*3=4+9=13
 
-const float NORMALIZED_WIDTH = 2.0f/sqrt(13);
-const float NORMALIZED_LENGTH = 3.0f/sqrt(13);
+const float NORMALIZED_WIDTH = 1.0f/sqrt(2);//2.0f/sqrt(13);
+const float NORMALIZED_LENGTH = 1.0f/sqrt(2);//3.0f/sqrt(13);
 
 const int ANALOG_RESOLUTION = 12;
-const int MAX_BRIGHTNESS = (1 << ANALOG_RESOLUTION) - 1;
+const int ANALOG_MAX = (1 << ANALOG_RESOLUTION) - 1;
 
 enum { 
   BACK_RIGHT  = 0, 
@@ -44,7 +44,7 @@ const float LED_COORDS[4][2] = {
 const int AUDIO_PIN = A6;
 
 void logarithmicWrite(int pin, float brightness) {
-  analogWrite(pin, pow(brightness, 3)*MAX_BRIGHTNESS);
+  analogWrite(pin, pow(brightness, 3) * ANALOG_MAX);
 }
 
 void writeToAllPins(int br, int fr, int fl, int bl) {
@@ -59,45 +59,133 @@ void writeToAllPins(int brightness) {
 }
 
 class Sensors : public Polls {
+  private:
+    float xAccelLowpass = 0.0f;
+    float xAccelBandpass = 0.0f;
+    float yAccelLowpass = 0.0f;
+    float yAccelBandpass = 0.0f;
+    float zAccelLowpass = 0.0f;
+    float zAccelBandpass = 0.0f;
+    
+    float accelLowpassEma = 0.85f;
+    float accelBandpassEma = 0.85f;
+
+    enum { xAccel, yAccel, zAccel,
+           xGyro, yGyro, zGyro };
+           
+    float readings[6];
+    int readingCount = 0;
+    
   public:
     Sensors(unsigned long updateDelay) : Polls(updateDelay) {
       if (!IMU.begin()) {
         Serial.println("Failed to initialize IMU!");
       }
     }
-  
-    bool update() override {
+
+    void read() {
+      if (!IMU.accelerationAvailable() || !IMU.gyroscopeAvailable()) return;
+
+      float newReadings[6];
+      IMU.readAcceleration(newReadings[xAccel], newReadings[yAccel], newReadings[zAccel]);
+      IMU.readGyroscope(newReadings[xGyro], newReadings[yGyro], newReadings[zGyro]);
+      for (int i = 0; i < 6; ++i) readings[i] += newReadings[i];
+
+      ++readingCount;
+    }
+
+    bool update() override {      
       if (!Polls::update()) return false;
-      
-      if (!IMU.accelerationAvailable() || !IMU.gyroscopeAvailable()) return false;
-  
-      enum { xAccel, yAccel, zAccel,
-             xGyro, yGyro, zGyro };
-      float readings[6];
-      IMU.readAcceleration(readings[xAccel], readings[yAccel], readings[zAccel]);
-      IMU.readGyroscope(readings[xGyro], readings[yGyro], readings[zGyro]);
-  
+
+      if (readingCount == 0) return false;
+
+      for (int i = 0; i < 6; ++i) readings[i] /= readingCount;
+
       //Serial.println("X:" + String(readings[xAccel]) + ",Y:" + String(readings[yAccel]) + ",Z:" + String(readings[zAccel]));
   
       xAccelLowpass = xAccelLowpass * accelLowpassEma + readings[xAccel] * (1 - accelLowpassEma);
+      float xAccelHighpass = readings[xAccel] - xAccelLowpass;
+      xAccelBandpass = xAccelBandpass * accelBandpassEma + xAccelHighpass * (1 - accelBandpassEma);
+
       yAccelLowpass = yAccelLowpass * accelLowpassEma + readings[yAccel] * (1 - accelLowpassEma);
-  
-      float len = sqrt(xAccelLowpass * xAccelLowpass + yAccelLowpass * yAccelLowpass);
-      float normalized[] = { -yAccelLowpass / len, -xAccelLowpass / len };
-  
+      float yAccelHighpass = readings[yAccel] - yAccelLowpass;
+      yAccelBandpass = yAccelBandpass * accelBandpassEma + yAccelHighpass * (1 - accelBandpassEma);
+
+      zAccelLowpass = zAccelLowpass * accelLowpassEma + readings[zAccel] * (1 - accelLowpassEma);
+      float zAccelHighpass = readings[zAccel] - zAccelLowpass;
+      zAccelBandpass = zAccelBandpass * accelBandpassEma + abs(zAccelHighpass) * (1 - accelBandpassEma);
+
+      float absHighpassSum = abs(xAccelHighpass) + abs(yAccelHighpass) + abs(zAccelHighpass);
+      
+      /*
+      Serial.println(
+         "Lowpass:"+String(zAccelLowpass)+
+        ",Highpass:"+String(zAccelHighpass)+
+        ",Bandpass:"+String(zAccelBandpass)+
+        ",HighpassSum:"+String(absHighpassSum));
+      */
+      Serial.print("accelerometer:" + String(25*absHighpassSum));
+      
+      float len = sqrt(xAccelBandpass * xAccelBandpass + yAccelBandpass * yAccelBandpass + zAccelBandpass * zAccelBandpass);
+      float normalized[] = { -yAccelBandpass / len, -xAccelBandpass / len, zAccelBandpass / len };
+
+      // Light up according to accelerometer data
       for (int i = 0; i < 4; ++i) {
-        float dot = LED_COORDS[i][0] * normalized[0] + LED_COORDS[i][1] * normalized[1];
+        //          X                                  Y                                  Z             Divide because all 4 lights are on together
+        float dot = LED_COORDS[i][0] * normalized[0] + LED_COORDS[i][1] * normalized[1] + normalized[2] / 3;
         logarithmicWrite(LED_PINS[i], dot * len);
       }
+
+      // Clear all accumulated readings
+      for (int i = 0; i < 6; ++i) readings[i] = 0.0f;
+      readingCount = 0;
+
+      return true;
+    }
+};
+
+class Sounds : public Polls {
+  private:
+    float reading = 0.0f;
+    int readingCount = 0;
+  
+    float soundLowpass = 0.0f;
+    
+    float soundLowpassEma = 0.85f;
+    
+  public:
+    Sounds(unsigned long updateDelay) : Polls(updateDelay) {
+
     }
   
-  private:
-    float xAccelLowpass = 0.0f;
-    float yAccelLowpass = 0.0f;
-    float accelLowpassEma = 0.8f;
+    void read() {
+      reading += abs(analogRead(AUDIO_PIN)) / ANALOG_MAX;
+      ++readingCount;
+    }
+
+    bool update() override {
+      //if (!Polls::update()) return false;
+
+      if (readingCount == 0) return false;
+
+      reading /= readingCount;
+      
+      //Serial.println("Volume:" + String(analogRead(AUDIO_PIN)));
+    
+      soundLowpass = soundLowpass * soundLowpassEma + reading * (1 - soundLowpassEma);
+      float soundHighpass = abs(reading - soundLowpass);
+
+      Serial.println(",sound:" + String(25*soundHighpass));
+
+      reading = 0.0f;
+      readingCount = 0;
+
+      return true;
+    }
 };
 
 Sensors* sensors;
+Sounds* sounds;
 
 void setup() {
   analogWriteResolution(12);
@@ -108,13 +196,16 @@ void setup() {
   randomSeed(analogRead(A5));
 
   sensors = new Sensors(16);
+  sounds = new Sounds(16);
 }
 
 void loop() {
     Polls::setMillis(millis());
 
-    sensors->update();
-    Serial.println("Volume:" + String(analogRead(AUDIO_PIN)));
+    sensors->read();
+    sounds->read();
 
-    delay(1);
+    if (sensors->update()) {
+      sounds->update();
+    }
 }
