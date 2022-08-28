509f8638104a02388a2686817c69c09fa6fda59a
Steven Dunn <thevgm01@gmail.com>
2022-08-28 18:00:02 -0500

    Added bonk detection

--- Bee_Lamp/Bee_Lamp.ino
+++ Bee_Lamp/Bee_Lamp.ino
@@ -15,15 +15,18 @@
 // 2:3
 // 2*2+3*3=4+9=13
 
+template<typename T>
+int sign(T val) { return (T(0) < val) - (val < T(0)); }
+
 const float NORMALIZED_WIDTH = 1.0f/sqrt(2);//2.0f/sqrt(13);
 const float NORMALIZED_LENGTH = 1.0f/sqrt(2);//3.0f/sqrt(13);
 
 const int ANALOG_RESOLUTION = 12;
 const int ANALOG_MAX = (1 << ANALOG_RESOLUTION) - 1;
 
-const float ACCEL_DEBUG_SCALE = 25.0f,
-            GYRO_DEBUG_SCALE  = 1.5f,
-            SOUND_DEBUG_SCALE = 10.0f;
+const float DEBUG_ACCEL_SCALE = 25.0f,
+            DEBUG_GYRO_SCALE  = 1.5f,
+            DEBUG_SOUND_SCALE = 10.0f;
             
 enum { 
   BACK_RIGHT  = 0, 
@@ -64,10 +67,29 @@ void writeToAllPins(int brightness) {
 }
 
 class Sensors : public Polls {
+  // If the highpass detects that all 3 sensors have hit their bonk threshold, toggle the light
+  // Then, wait until all three bandpasses have returned to under a different threshold for a period of time before allowing another bonk
+  
   private:
+    const float 
+      accelThresholdHigh = 1.5f / DEBUG_ACCEL_SCALE, accelThresholdLow = 0.03f / DEBUG_ACCEL_SCALE,
+      gyroThresholdHigh  = 1.5f / DEBUG_GYRO_SCALE,  gyroThresholdLow  = 0.20f / DEBUG_GYRO_SCALE,
+      soundThresholdHigh = 0.8f / DEBUG_SOUND_SCALE, soundThresholdLow = 0.01f / DEBUG_SOUND_SCALE;
+
+    bool readyForBonk = false;
+    int accelCounter = 0;
+    int gyroCounter = 0;
+    int soundCounter = 0;
+
+    const float 
+      bonkGracePeriod = 0.1f, // Seconds
+      bonkSettlePeriod = 0.1f;
+
+    const int bonkGraceLoops, bonkSettleLoops;
+  
     float accelLowpassEma  = 0.85f, accelBandpassEma = 0.85f,
           gyroLowpassEma   = 0.85f, gyroBandpassEma  = 0.85f,
-          soundLowpassEma = 0.85f,  soundBandpassEma = 0.85f;
+          soundLowpassEma  = 0.85f, soundBandpassEma = 0.85f;
 
     int readingCount = 0;
     int soundReadingCount = 0;
@@ -77,6 +99,9 @@ class Sensors : public Polls {
       xGyro = 3, yGyro = 4, zGyro = 5, 
 
       sound = 6,
+
+      accelAbsSum = 7,
+      gyroAbsSum = 8,
 
       reading  = 0 << 6,
       lowpass  = 1 << 6,
@@ -89,7 +114,7 @@ class Sensors : public Polls {
     void runHighpass(float &highpass, const float lowpass, const float reading) { highpass = reading - lowpass; }
     void runHighpassAbs(float &highpass, const float lowpass, const float reading) { highpass = abs(reading - lowpass); }
     void runBandpass(float &bandpass, const float highpass, const float ema) { runLowpass(bandpass, highpass, ema); }
-    
+
     void runFullpass(const float reading, float &lowpass, float &highpass, float &bandpass, const float lowpassEma, const float bandpassEma) {
       runLowpass(lowpass, reading, lowpassEma);
       runHighpass(highpass, lowpass, reading);
@@ -111,11 +136,17 @@ class Sensors : public Polls {
     }
     
   public:
-    Sensors(unsigned long updateDelay) : Polls(updateDelay) {
+    Sensors(unsigned long updateDelay) : 
+        Polls(updateDelay), 
+        bonkGraceLoops(bonkGracePeriod * 1000.0f / (float)updateDelay),
+        bonkSettleLoops(bonkSettlePeriod * 1000.0f / (float)updateDelay) {
+          
       if (!IMU.begin()) {
         Serial.println("Failed to initialize IMU!");
       }
+      
       pinMode(AUDIO_PIN, INPUT);
+      
     }
 
     void read() {
@@ -149,28 +180,55 @@ class Sensors : public Polls {
       runFullpass(yAccel, accelLowpassEma, accelBandpassEma);
       runFullpass(zAccel, accelLowpassEma, accelBandpassEma);
       
-      float accelAbsLowpassSum  = abs(map[xAccel|lowpass])  + abs(map[yAccel|lowpass])  + abs(map[zAccel|lowpass]);
-      float accelAbsHighpassSum = abs(map[xAccel|highpass]) + abs(map[yAccel|highpass]) + abs(map[zAccel|highpass]);
-      float accelAbsBandpassSum = abs(map[xAccel|bandpass]) + abs(map[yAccel|bandpass]) + abs(map[zAccel|bandpass]);
+      map[accelAbsSum|lowpass]  = abs(map[xAccel|lowpass])  + abs(map[yAccel|lowpass])  + abs(map[zAccel|lowpass]);
+      map[accelAbsSum|highpass] = abs(map[xAccel|highpass]) + abs(map[yAccel|highpass]) + abs(map[zAccel|highpass]);
+      map[accelAbsSum|bandpass] = abs(map[xAccel|bandpass]) + abs(map[yAccel|bandpass]) + abs(map[zAccel|bandpass]);
 
       runFullpass(xGyro, gyroLowpassEma, gyroBandpassEma);
       runFullpass(yGyro, gyroLowpassEma, gyroBandpassEma);
       runFullpass(zGyro, gyroLowpassEma, gyroBandpassEma);
 
-      float gyroAbsHighpassSum = abs(map[xGyro|highpass]) + abs(map[yGyro|highpass]) + abs(map[zGyro|highpass]);
+      map[gyroAbsSum|lowpass]  = abs(map[xGyro|lowpass])  + abs(map[yGyro|lowpass])  + abs(map[zGyro|lowpass]);
+      map[gyroAbsSum|highpass] = abs(map[xGyro|highpass]) + abs(map[yGyro|highpass]) + abs(map[zGyro|highpass]);
+      map[gyroAbsSum|bandpass] = abs(map[xGyro|bandpass]) + abs(map[yGyro|bandpass]) + abs(map[zGyro|bandpass]);
 
       runFullpassAbs(sound, soundLowpassEma, soundBandpassEma);
-      /*
-      Serial.println(
-         "Lowpass:"+String(zAccelLowpass)+
-        ",Highpass:"+String(zAccelHighpass)+
-        ",Bandpass:"+String(zAccelBandpass)+
-        ",HighpassSum:"+String(accelAbsHighpassSum));
-      */
-      Serial.print("accelerometer:" + String(ACCEL_DEBUG_SCALE*accelAbsHighpassSum));
-      Serial.print(",gyroscope:" + String(GYRO_DEBUG_SCALE*gyroAbsHighpassSum));
-      Serial.println(",sound:" + String(SOUND_DEBUG_SCALE*map[sound|highpass]));
 
+      if (readyForBonk) {
+        if (map[accelAbsSum|highpass] >= accelThresholdHigh) accelCounter += 2;
+        if (map[gyroAbsSum|highpass]  >= gyroThresholdHigh)  gyroCounter  += 2;
+        if (map[sound|highpass]       >= soundThresholdHigh) soundCounter += 2;
+  
+        if (accelCounter > 0 && gyroCounter > 0 && soundCounter > 0) {
+          Serial.println("Bonk!");
+          readyForBonk = false;
+        }
+        else if (accelCounter > bonkGraceLoops || gyroCounter > bonkGraceLoops || soundCounter > bonkGraceLoops) {
+          Serial.println("Held");
+          readyForBonk = false;
+        }
+      }
+      if (!readyForBonk) {
+        if (map[accelAbsSum|bandpass] >= accelThresholdLow) accelCounter = bonkSettleLoops;
+        if (map[gyroAbsSum|bandpass]  >= gyroThresholdLow)  gyroCounter  = bonkSettleLoops;
+        if (map[sound|bandpass]       >= soundThresholdLow) soundCounter = bonkSettleLoops;
+
+        if (accelCounter == 0 && gyroCounter == 0 && soundCounter == 0) {
+          Serial.println("Ready for bonk");
+          readyForBonk = true;
+        }
+      }
+
+      accelCounter = max(accelCounter - 1, 0);
+      gyroCounter  = max(gyroCounter  - 1, 0);
+      soundCounter = max(soundCounter - 1, 0);
+
+      if (false) {
+        Serial.print("accelerometer:" + String(DEBUG_ACCEL_SCALE*map[accelAbsSum|highpass]));
+        Serial.print(",gyroscope:" + String(DEBUG_GYRO_SCALE*map[gyroAbsSum|highpass]));
+        Serial.println(",sound:" + String(DEBUG_SOUND_SCALE*map[sound|highpass]));
+      }
+      
       float len = sqrt(map[xAccel|bandpass] * map[xAccel|bandpass] + map[yAccel|bandpass] * map[yAccel|bandpass] + map[zAccel|bandpass] * map[zAccel|bandpass]);
       float normalized[] = { -map[yAccel|bandpass] / len, -map[xAccel|bandpass] / len, map[zAccel|bandpass] / len };
 
